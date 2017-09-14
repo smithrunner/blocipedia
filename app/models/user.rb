7024544ff14ebb4eb5658f305ede21c1c5514350
Sam Smith <smithrunner@live.com>
2017-09-14 16:06:33 +0000

    user roles complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
+  has_many :wikis, dependent: :destroy
+  
+  enum role: [:user, :premium, :admin]
+  after_initialize :set_default_role, :if=> :new_record?
+  
+  def set_default_role
+    self.role ||= :user
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :wikis, dependent: :destroy
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    
   get 'welcome/index'
 
   get 'welcome/about'
